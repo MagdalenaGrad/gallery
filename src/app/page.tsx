c65db1552157f1bc015e18f4a93b2f27ce17be3e
Magda <mgdln.grad@gmail.com>
2024-09-01 18:40:31 +0200

    add basic ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+const urls = [
+  'https://utfs.io/f/ce49e071-59a9-4a20-a9a0-8432d604fc61-herpjl.webp',
+  'https://utfs.io/f/263c71df-4d87-4cca-8788-1072d9b1728c-lqwsp1.jpg',
+  'https://utfs.io/f/67321a9b-575b-4421-89df-c75de24269c1-ilen2j.jpg',
+];
+
+const mockImages = urls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
+const placeholderImgs = Array(7).fill(mockImages).flat();
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      hello
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {placeholderImgs.map((image) => (
+          <div
+            key={image.id}
+            className="w-48 h-48 aspect-[1] overflow-hidden">
+              <img
+                src={image.url}
+                alt="image"
+                className="w-full h-full object-cover"
+              />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
